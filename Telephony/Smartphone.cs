fc50381c1ec9ef869997acc241c78aa712b54a9a
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-19 19:34:49 +0300

    Telephon

---
+++ Telephony/Smartphone.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephony
+{
+   public class Smartphone: ICallable, IBrowsable
+    {
+        public string Call(string number)
+        {
+            if (!number.All(char.IsDigit))
+            {
+                throw new ArgumentException("Invalid number!");
+            }
+            return $"Calling... {number}";
+        }
+
+        public string Browse(string url)
+        {
+            if (url.Any(char.IsDigit))
+            {
+                throw new ArgumentException("Invalid URL!");
+            }
+            return $"Browsing: {url}!";
+        }
+    }
+}
---
+++ Telephony/StartUp.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telephony
+{
+   public class StartUp
+    {
+       public static void Main()
+       {
+           string[] numbers = Console.ReadLine().Split();
+           string[] urls = Console.ReadLine().Split();
+           Smartphone phone = new Smartphone();
+
+           foreach (var number in numbers)
+           {
+               try
+               {
+                    Console.WriteLine(phone.Call(number)); 
+               }
+               catch (Exception e)
+               {
+                   Console.WriteLine(e.Message);
+                   
+               }
+           }
+
+           foreach (var url in urls)
+           {
+               try
+               {
+                   Console.WriteLine(phone.Browse(url));
+                  
+               }
+               catch (Exception e)
+               {
+                   Console.WriteLine(e.Message);
+                  
+               }
+           }
+       }
+    }
+}
