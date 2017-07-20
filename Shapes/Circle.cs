d149216bf5b86b4c7c037df6e503f3f08a3aff52
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-20 16:30:28 +0300

    Shapes

---
+++ Shapes/Circle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Circle : IDrawable
+{
+    private double radius;
+
+    public Circle(double radius)
+    {
+        this.Radius = radius;
+    }
+
+    public double Radius
+    {
+        get { return this.radius; }
+        private set { this.radius = value; }
+    }
+    public void Draw()
+    {
+        double r_In = this.Radius - 0.4;
+        double r_Out = this.Radius + 0.4;
+
+        for (double y = this.Radius; y >= -this.Radius; --y)
+        {
+            for (double x = -this.Radius; x < r_Out; x += 0.5)
+            {
+                double value = x * x + y * y;
+
+                if (value >= r_In * r_In && value <= r_Out * r_Out)
+                {
+                    Console.Write("*");
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+
+        }
+    }
+}
+
---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Rectangle : IDrawable
+{
+    private double height;
+    private double width;
+
+    public Rectangle(double width, double height)
+    {
+        this.Width = width;
+        this.Height = height;
+
+    }
+
+    public double Width
+    {
+        get { return this.width; }
+        private set { this.width = value; }
+    }
+
+    public double Height
+    {
+        get { return this.height; }
+        private set { this.height = value; }
+    }
+    public void Draw()
+    {
+        this.DrawLine(this.Width, '*', '*');
+
+        for (int i = 1; i < this.Height - 1; ++i)
+        {
+            this.DrawLine(this.Width, '*', ' ');
+        }
+
+        this.DrawLine(this.Width, '*', '*');
+    }
+
+    private void DrawLine(double width, char end, char mid)
+    {
+        Console.Write(end);
+
+        for (int i = 1; i < width - 1; ++i)
+        {
+            Console.Write(mid);
+        }
+
+        Console.WriteLine(end);
+    }
+}
+
