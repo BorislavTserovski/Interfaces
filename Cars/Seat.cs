25a0e89c76d8dbfd0f2a665559803a7790511e8a
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-20 16:46:03 +0300

    Cars

---
+++ Cars/Seat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Seat: ICar
+{
+    public string Model { get; }
+    public string Color { get; }
+
+    public Seat(string model, string color)
+    {
+        this.Model = model;
+        this.Color = color;
+    }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendLine($"{this.Color} Seat {this.Model}");
+        sb.AppendLine(this.Start());
+        sb.Append(this.Stop());
+        return sb.ToString();
+    }
+}
+
---
+++ Cars/Tesla.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Tesla:ICar, IElectricCar
+{
+    public string Model
+    {
+        get;
+    }
+    public string Color { get; }
+    public int Battery { get; }
+    public Tesla(string model, string color, int batteries)
+    {
+        this.Model = model;
+        this.Color = color;
+        this.Battery = batteries;
+    }
+    public string Start()
+    {
+        return "Engine start";
+    }
+
+    public string Stop()
+    {
+        return "Breaaak!";
+    }
+
+  
+
+    public override string ToString()
+    {
+        StringBuilder sb = new StringBuilder();
+         sb.AppendLine($"{this.Color} Tesla {this.Model} with {this.Battery} Batteries");
+        sb.AppendLine(this.Start());
+        sb.Append(this.Stop());
+        return sb.ToString();
+    }
+}
+
